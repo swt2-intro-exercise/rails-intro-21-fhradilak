73c76c0108a13867af0dd1a40f97a1dcb6858953
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2021-11-20 12:07:53 +0100

    add failing test #4 migrate DB

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,8 +1,11 @@
 require 'rails helper'
 
 describe "Author Model", type: :model do
-  it "should have the method name wich retruns the full name of an author" do
-    author = Author.new(first_name: "Herbert")
+  it "should have the attributes first and last name and website and the method name wihch returns the full name of an author" do
+    author = Author.new(first_name: "Herbert", last_name: "Herbertson", website: "Herbertson.com")
     expect(author.first_name).to eq("Herbert")
+    expect(author.last_name).to eq("Herbertson")
+    expect(author.website).to eq("Herbertson.com")
+    expect(author.name).to eq("Herbert Herbertson")
   end
 end
