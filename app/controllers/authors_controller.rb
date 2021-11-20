649e7d959529676b62850e00aec7b65b759514e2
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2021-11-20 13:00:50 +0100

    add test for #5 add missing author files

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,19 @@
+class AuthorsController < ApplicationController
+  def show
+    @author = Author.find(params[:id])
+  end
+  def new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    @author.save
+    redirect_to @author
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :text, :last_name, :text, :homepage, :text)
+  end
+end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
  describe "New author page", type: :feature do
+
+     first_name = 'Alanson'
+     last_name = 'Turingson'
+     homepage = 'http://wikipedia.org/Alan_Turing'
+
    it "should exist at 'new_author_path' and render without error" do
      # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
      visit new_author_path
@@ -12,5 +17,15 @@ require 'rails_helper'
      expect(page).to have_field(name: 'author[first_name]')
      expect(page).to have_field(name: 'author[last_name]')
      expect(page).to have_field(name: 'author[homepage]')
+   end
+
+   it "should submit a new author" do
+     visit new_author_path
+     page.fill_in 'author[first_name]', with: first_name
+     page.fill_in 'author[last_name]', with: last_name
+     page.fill_in 'author[homepage]', with: homepage
+     find('input[type="submit"]').click
+
+     expect(Author.exists?(first_name: first_name, last_name: last_name, homepage: homepage)).to be_truthy
    end
  end
