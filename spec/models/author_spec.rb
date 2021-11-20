34941dc14006422353709ab20aec61a0c84dc50e
franziska.hradilak <franziska.hradilak@student.hpi.uni-potsdam.de>
2021-11-20 12:45:14 +0100

    fix test and make it run

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,11 @@
-require 'rails helper'
+require 'rails_helper'
 
 describe "Author Model", type: :model do
   it "should have the attributes first and last name and website and the method name wihch returns the full name of an author" do
-    author = Author.new(first_name: "Herbert", last_name: "Herbertson", website: "Herbertson.com")
+    author = Author.new(first_name: "Herbert", last_name: "Herbertson", homepage: "Herbertson.com")
     expect(author.first_name).to eq("Herbert")
     expect(author.last_name).to eq("Herbertson")
-    expect(author.website).to eq("Herbertson.com")
+    expect(author.homepage).to eq("Herbertson.com")
     expect(author.name).to eq("Herbert Herbertson")
   end
 end
